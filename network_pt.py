76c031a061229d48e1651cb97f371edb95e794c8
liuxianyong <380221409@qq.com>
2025-01-08 13:02:37 +0000

    support gpu

--- network_pt.py
+++ network_pt.py
@@ -12,25 +12,30 @@ from mnist_loader import MNISTDataset
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda:0" if use_cuda else "cpu")
 torch.backends.cudnn.benchmark = True
+numgpus =  torch.cuda.device_count()
 
 class NetWork_PT(nn.Module):
     # 模型参数
-    def __init__(self, x_dim):
+    def __init__(self, x_dim, network_ac_type):
         super().__init__()
         self.hidden1 = nn.Linear(x_dim, 30)
         self.hidden2 = nn.Linear(30, 10)
-        
+        self.network_ac_type = network_ac_type
         # self.net = nn.Sequential(nn.Linear(x_dim, 30), F.sigmoid(), nn.Linear(30, 10), nn.sigmoid())
 
      # 模型的前向传播，即如何根据输入X返回所需的模型输出
     def forward(self, X):
-        activation = nn.Sigmoid()
+        if self.network_ac_type == 'Sigmoid':
+            activation = nn.Sigmoid()
+        else:
+            activation = nn.ReLU()
+
         return self.hidden2(activation(self.hidden1(X)))
 
 # Parameters
 params = {'batch_size': 10,
           'shuffle': True,
-          'num_workers': 6}
+          'num_workers': 6 if numgpus <=0 else 4*numgpus}
 
 max_epochs = 10
 learning_rate = 1e-3
@@ -48,12 +53,17 @@ def mnist_dataloader():
 
     return train_dataloader, validate_dataloader, test_dataloader
 
-def train(loss_fn_type = 'CrossEntropyLoss'):
-    model = NetWork_PT(784)
+def train(loss_fn_type = 'CrossEntropyLoss', network_ac_type = 'Sigmoid'):
+    model = NetWork_PT(784, network_ac_type)
+
+    if numgpus >= 1:
+        model.cuda()
+        model = nn.DataParallel(model, device_ids=list(range(numgpus)), dim=0)
+        
     if loss_fn_type == 'CrossEntropyLoss':
-        loss_fn = torch.nn.CrossEntropyLoss()
+        loss_fn = torch.nn.CrossEntropyLoss() if numgpus <=0 else torch.nn.CrossEntropyLoss().cuda()
     else:
-        loss_fn = torch.nn.MSELoss(reduction='sum')
+        loss_fn = torch.nn.MSELoss(reduction='sum') if numgpus <=0 else torch.nn.MSELoss(reduction='sum').cuda()
 
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
@@ -67,16 +77,17 @@ def train(loss_fn_type = 'CrossEntropyLoss'):
         metric = d2l.Accumulator(3)
         for i, data in enumerate(train_dataloader, 0):
             inputs, labels = data
-
-            optimizer.zero_grad()
+            inputs.squeeze_()
+            labels.squeeze_()
 
             # forward + backward + optimize
-            outputs = model(inputs.squeeze())
-            labels = labels.squeeze().float()
-            loss = loss_fn(outputs, labels)
- 
+            outputs = model(inputs)
+
+            loss = loss_fn(outputs.cuda(), labels.float().cuda())
             loss.backward()
+
             optimizer.step()
+            optimizer.zero_grad()
 
             # print statistics
             # running_loss += loss.item()
@@ -85,8 +96,8 @@ def train(loss_fn_type = 'CrossEntropyLoss'):
             #     running_loss = 0.0
 
             # visulization of results.
-            labels = d2l.argmax(labels, axis=1)
-            metric.add(float(loss), d2l.accuracy(outputs, labels), labels.numel())
+            labels = d2l.argmax(labels, axis=1).cpu()
+            metric.add(float(loss.cpu()), d2l.accuracy(outputs.cpu(), labels), labels.numel())
 
         test_acc = d2l.evaluate_accuracy(model, test_dataloader)
         metrics = (metric[0] / metric[2], metric[1] / metric[2], test_acc,)
